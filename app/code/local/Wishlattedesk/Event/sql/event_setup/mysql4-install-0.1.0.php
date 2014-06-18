f523ba12d542ef84229dbbdeba2377ff617317ff
hihihehe <thethi.thichthat@gmail.com>
2014-06-18 15:26:28 +0700

    Change table col - webform_id to event_id

--- app/code/local/Wishlattedesk/Event/sql/event_setup/mysql4-install-0.1.0.php
+++ app/code/local/Wishlattedesk/Event/sql/event_setup/mysql4-install-0.1.0.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Wishlattedesk_Customerrelationship
+ * Wishlattedesk_Event
  *
  * @category    Wishlattedesk
- * @package     Wishlattedesk_Customerrelationship
+ * @package     Wishlattedesk_Event
  * @copyright   Copyright (c) 2014 Wishlattedesk Inc. (http://www.magentocommerce.com)
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  * @author      Hieu Nguyen (Wishlattedesk's team)
